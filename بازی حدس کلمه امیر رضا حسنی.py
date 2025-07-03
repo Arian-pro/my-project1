373cd4da6f5cdca0cec78ea5757bb1259bc8af6e
gsrtntuiyoyhi9pedtoK{"fukdtuerzxtg789 <aliddthty8ozt7irtz@gmail.com>
2025-07-03 08:36:16 +0330

    Update بازی حدس کلمه امیر رضا حسنی.py

--- بازی حدس کلمه امیر رضا حسنی.py
+++ بازی حدس کلمه امیر رضا حسنی.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+print('**************')
 
 #=====List of words=====
 words = ['red','blue','yellow','green','orange','purple','pink','brown','black','gray',]
